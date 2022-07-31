d94f95339da2296ba82cf81f9f414a137fbc5f20
ymh <hanyongming@gmail.com>
2022-07-30 21:40:37 -0400

    Haskell book state exercises

--- haskell-book/chp23-state/StateExs.hs
+++ haskell-book/chp23-state/StateExs.hs
@@ -3,9 +3,9 @@ module StateExs where
 
 import Control.Applicative
 import Data.Maybe
-import Hedgehog
-import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
+-- import Hedgehog
+-- import qualified Hedgehog.Gen as Gen
+-- import qualified Hedgehog.Range as Range
 import Data.Char
 
 newtype State s a = State { runState :: s -> (a, s) }
@@ -36,3 +36,23 @@ instance Monad (State s) where
                                       (b, s'') = sb s'
                                   in (b, s''))
 
+------
+
+-- Construct a State where the state is also the value you return
+get :: State s s
+get = State $ \s -> (s, s)
+
+
+-- Construct a State where the resulting state is the argument provided, and the value defaults to unit:
+put :: s -> State s ()
+put s = State $ \_ -> ((), s)
+
+-- Run the State with s and get the state that results
+exec :: State s a -> s -> s
+exec (State sa) s = let (_, s') = sa s in s'
+
+-- Run the State with s and get the value that results
+eval :: State s a -> s -> a
+eval (State sa) = \s -> fst (sa s)
+
+modify :: (s -> s) -> State s ()
