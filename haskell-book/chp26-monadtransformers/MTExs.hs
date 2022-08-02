46c4ba68009d12871c8f3eda470e19dfcd8d4e39
ymh <hanyongming@gmail.com>
2022-08-02 13:26:02 -0400

    StateT applicative and monad instances

--- haskell-book/chp26-monadtransformers/MTExs.hs
+++ haskell-book/chp26-monadtransformers/MTExs.hs
@@ -2,6 +2,7 @@
 
 module MTExs where
 
+import Control.Arrow (first, second)
 import Control.Applicative
 import Control.Monad (ap)
 
@@ -34,3 +35,37 @@ instance Monad m
             Left e -> return (Left e)
             Right a -> runEitherT (f a)   
 
+--- StateT
+
+newtype StateT s m a = 
+  StateT { runStateT :: s -> m (a, s) }
+
+instance (Functor m) => Functor (StateT s m) where
+    fmap :: Functor m => (a -> b) -> StateT s m a -> StateT s m b
+    fmap f (StateT smas) = StateT $ \s -> 
+                                      let mas = smas s
+                                      in fmap (first f) mas 
+
+
+instance (Monad m)
+    => Applicative (StateT s m) where
+  pure :: Monad m => a -> StateT s m a
+  pure a = StateT (\s -> pure (a, s)) 
+  (<*>) :: Monad m => StateT s m (a -> b) -> StateT s m a -> StateT s m b
+  (StateT smaf) <*> (StateT sma) = StateT (\s -> do 
+                                                    (f, s') <- smaf s 
+                                                    (a, s'') <- sma s'
+                                                    return (f a, s'')) 
+
+
+instance (Monad m)
+    => Monad (StateT s m) where
+  return :: Monad m => a -> StateT s m a
+  return = pure
+
+  (>>=) :: Monad m => StateT s m a -> (a -> StateT s m b) -> StateT s m b
+  (StateT sma) >>= f = StateT $ \s -> do 
+                                         (a, s') <- sma s
+                                         let ssmb = f a
+                                         (b, s'') <- (runStateT ssmb) s'
+                                         return (b, s'')
